507e4844220d15915294be8b77982d3ba0ea12bc
Marco Lozada <zero_could00@hotmail.com>
2012-07-04 18:37:43 -0500

    creación
    Se creo la estructura base

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SIPREMOD</title>
+<link rel="stylesheet" type="text/css" href="lib/css/reset.css" />
+<link rel="stylesheet" type="text/css" href="lib/css/template.css" />
+<script type="text/javascript" language="javascript" src="lib/js/jquery-1.7.min.js"></script>
+</head>
+<body>
+<div class="top">
+	<ul class="menu_base">
+    	<li><a href="#">Inicio</a></li>
+        <li><a href="#">Polizas</a>
+        	<ul>
+            	<li><a href="">Nueva</a></li>
+                <li><a href="">Aprovaciones</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Catalogos</a>
+        	<ul>
+            	<li><a href="">Cuentas</a></li>
+                <li><a href="">Dependencias</a></li>
+                <li><a href="">Fondos</a></li>
+                <li><a href="">Claves Programaticas</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Usuarios</a></li>
+    </ul>
+</div>
+<div class="centro fondoPrincipal bordes centrado anchoBase">
+	<p>Contenido</p>
+</div>
+<div class="fondo">
+	<p>Fondo</p>
+</div>
+</body>
+</html>
---
+++ lib/php/conexion.php
@@ -0,0 +1,6 @@
+<?
+	mysql_connect("localhost","root");
+	mysql_select_db("proclimme_db");
+	/*mysql_connect("proclimmesql.db.9243612.hostedresource.com","proclimmeSQLread","readSQL_2012");
+	mysql_select_db("proclimmesql")*/
+?>
